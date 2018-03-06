ad07cad86f40c192e849ba6d258b2c54ba2e4330
Mehul Sampat <mpsampat@gmail.com>
2018-03-06 23:13:09 +0000

    update config file

--- bowl_config.py
+++ bowl_config.py
@@ -18,11 +18,11 @@ class BowlConfig(Config):
 
     # Use small images for faster training. Set the limits of the small side
     # the large side, and that determines the image shape.
-    IMAGE_MIN_DIM = 512
-    IMAGE_MAX_DIM = 512
+    IMAGE_MIN_DIM = 256
+    IMAGE_MAX_DIM = 256
 
     # Use smaller anchors because our image and objects are small
-    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64)  # anchor side in pixels
 
     # Reduce training ROIs per image because the images are small and have
     # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
@@ -33,7 +33,7 @@ class BowlConfig(Config):
     # use small validation steps since the epoch is small
     VALIDATION_STEPS = 5
 
-    USE_MINI_MASK = True
+    USE_MINI_MASK = False
 
     MAX_GT_INSTANCES = 256
 
